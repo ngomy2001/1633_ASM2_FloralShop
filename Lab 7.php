dcfb02814f3e18e14207d3bc6ddad4ec5a0be87d
Jade Ngo <ngomy2001@gmail.com>
2021-02-05 09:33:50 +0700

    Add files via upload
    Example Lab 7

---
+++ Lab 7.php
@@ -0,0 +1,57 @@
+<html>
+	<head>
+		<title>PHP Test</title>
+	</head>
+	<body>
+		<?php 
+			echo '<p>TEST HEROKU POSTGRESQL DATABASE </p>'; 
+			# Heroku credential 
+			$host_heroku = "ec2-54-172-17-119.compute-1.amazonaws.com";
+			$db_heroku = "d2o7k1804l4mfj";
+			$user_heroku = "vfxjknqslqemhw";
+			$pw_heroku = "30f86ff86156a0d419db8d4f2ebdaf2ce7f6ad779e51877a31159127a9919f53";
+			# Create connection to Heroku Postgres
+			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+			$pg_heroku = pg_connect($conn_string);
+			
+			if (!$pg_heroku)
+			{
+				die('Error: Could not connect: ' . pg_last_error());
+			}
+			# Get data by query
+			$query = 'select * from users';
+			$result = pg_query($pg_heroku, $query);
+			# Display data column by column
+			$i = 0;
+			echo '<html><body><table><tr>';
+			while ($i < pg_num_fields($result))
+			{
+				$fieldName = pg_field_name($result, $i);
+				echo '<td>' . $fieldName . '</td>';
+				$i = $i + 1;
+			}
+			echo '</tr>';
+			# Display data row by row
+			$i = 0;
+			while ($row = pg_fetch_row($result)) 
+			{
+				echo '<tr>';
+				$count = count($row);
+				$y = 0;
+				while ($y < $count)
+				{
+					$c_row = current($row);
+					echo '<td>' . $c_row . '</td>';
+					next($row);
+					$y = $y + 1;
+				}
+				echo '</tr>';
+				$i = $i + 1;
+			}
+			pg_free_result($result);
+
+			echo '</table></body></html>';
+
+		?> 
+	</body>
+</html>
